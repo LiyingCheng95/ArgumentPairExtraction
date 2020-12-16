3da58220fba2ba59d82015341894b6df1543459f
LiyingCheng95 <chengliying9581@gmail.com>
2020-12-16 20:41:18 +0800

    Add files via upload

--- data_processing/dataProcessing.py
+++ data_processing/dataProcessing.py
@@ -12,19 +12,22 @@ from transformers import *
 import re
 import torch
 import numpy as np
+from collections import defaultdict
 			
 def sep_sent(s):
 	nlp = English()
 
-	sentencizer = nlp.create_pipe("sentencizer")
-	nlp.add_pipe(sentencizer)
+	# sentencizer = nlp.create_pipe("sentencizer")
+	# nlp.add_pipe(sentencizer)
+	nlp.add_pipe(nlp.create_pipe('sentencizer'))
 	s = s.replace('by Yu et al.', 'by Yu et al..').replace('by Wen et al.', 'by Wen et al..').replace('et. al.','et al').replace('et al.,','et al').replace('et al.','et al')
 	# s = re.sub('et al. [^A-Z]]','et al [A-Z]',s)
 	doc = nlp(s)
 	doc = list(doc.sents)
+	# doc = [sent.string for sent in doc.sents]
 	return doc
 
-def tagging_sequence(outputfile,corpus,arguments_corpus,category):
+def tagging_sequence(outputfile,corpus,arguments_corpus,category,paperid):
 	bio=0
 	vecs=[]
 	sents=[]
@@ -60,13 +63,26 @@ def tagging_sequence(outputfile,corpus,arguments_corpus,category):
 		temp+='\t'
 		i=str(i).replace('\n','')
 
+		i = i.strip() # Added by TY
+		if i == '':
+			continue
+
 
 		for j in arguments_corpus:
 			# print("j: ",j[1])
 			# print("i: ",i)
-			
-			if i in j[1].replace('et. al.','et al ') and i!='':
-				# print(j2tag,j[2])
+			# argument = j[1].replace('et. al.','et al ')
+			argument = j[1].replace('by Yu et al.', 'by Yu et al..').replace('by Wen et al.', 'by Wen et al..').replace('et. al.','et al').replace('et al.,','et al').replace('et al.','et al')
+			argument = argument.strip() # added by TY
+			argument = re.sub(r'^\. ', '', argument)
+			try:
+				sents = sep_sent(argument)
+			except:
+				print('argument with problem: ', argument)
+				break
+			if (i in argument \
+				or str(sep_sent(argument)[0]) in i) \
+				and i!='':
 				if j2tag!=j[2] or bio==0:
 
 					temp+='B-'
@@ -85,10 +101,76 @@ def tagging_sequence(outputfile,corpus,arguments_corpus,category):
 				j2tag=j[2]
 				temp += '\t'
 				temp += category
+				temp += '\t'
+				temp += str(paperid)
 				temp+='\n'
 				outputfile.write(temp)
 				flag=1
 				break
+
+
+
+
+			# try:
+			# 	# if str(sep_sent(argument)[0]) in i:
+			# 	# 	print('corpus: ', i)
+			# 	# 	print('first sent of arguemnt: ', str(sep_sent(argument)[0]))
+			# 	if (i in argument \
+			# 		or str(sep_sent(argument)[0]) in i ) \
+			# 		and i!='':
+			# 		# print(j2tag,j[2])
+			# 		if j2tag!=j[2] or bio==0:
+
+			# 			temp+='B-'
+			# 			temp+=category
+			# 			temp+='\t'
+			# 			temp+='B-'
+			# 			bio=1
+			# 		else:
+			# 			temp+='I-'
+			# 			temp+=category
+			# 			temp+='\t'
+			# 			temp+='I-'
+
+
+			# 		temp+=j[2]
+			# 		j2tag=j[2]
+			# 		temp += '\t'
+			# 		temp += category
+			# 		temp += '\t'
+			# 		temp += str(paperid)
+			# 		temp+='\n'
+			# 		outputfile.write(temp)
+			# 		flag=1
+			# 		break
+			# except:
+			# 	print('argument with problem: ', argument)
+			# 	if i in argument and i!='':
+			# 		# print(j2tag,j[2])
+			# 		if j2tag!=j[2] or bio==0:
+
+			# 			temp+='B-'
+			# 			temp+=category
+			# 			temp+='\t'
+			# 			temp+='B-'
+			# 			bio=1
+			# 		else:
+			# 			temp+='I-'
+			# 			temp+=category
+			# 			temp+='\t'
+			# 			temp+='I-'
+
+
+			# 		temp+=j[2]
+			# 		j2tag=j[2]
+			# 		temp += '\t'
+			# 		temp += category
+			# 		temp += '\t'
+			# 		temp += str(paperid)
+			# 		temp+='\n'
+			# 		outputfile.write(temp)
+			# 		flag=1
+			# 		break
 		if flag==0:
 			temp+='O'
 			temp+='\t'
@@ -97,18 +179,44 @@ def tagging_sequence(outputfile,corpus,arguments_corpus,category):
 			temp+='O'
 			temp += '\t'
 			temp += category
+			temp += '\t'
+			temp += str(paperid)
 			temp+='\n'
 			outputfile.write(temp)
 
+def update_file1():
+	with open('file1new.csv', 'w') as outputfile:
+		outputfile = csv.writer(outputfile)
+		with open('iclr_pairs (updated).csv') as iclr_pairs:
+			iclr_pairs = csv.reader(iclr_pairs, delimiter=',')
+			iclr_pairs = list(iclr_pairs)
+		
+			with open('file1.csv') as csvfile1:
+				file1 = csv.reader(csvfile1, delimiter=',') 
+
+				for line in file1:
+					# arguments_review=[ file2[index] for index in [i for i, e in enumerate(file2) if e[0] == line[0] and e[-1]=="Review"] ]
+					# arguments_reply=[ file2[index] for index in [i for i, e in enumerate(file2) if e[0] == line[0] and e[-1]=="Reply"] ]
+					# # print(line)
+					# # print(arguments_review)
+					sep=line[1].find("——————【reply】——————")
+					review=line[1][22:sep-2]
+					reply=line[1][sep+21:-1]
+					for row in iclr_pairs:
+						if review == row[-1]:
+							paperid = row[1]
+					line.append(paperid)
+					outputfile.writerow(line)
+
 
 def main():
-	with open('ReviewRebuttalnew.txt', 'w') as outputfile:
+	with open('ReviewRebuttalnew2.txt', 'w') as outputfile:
 
 		with open('file2.csv') as csvfile2:
 			file2 = csv.reader(csvfile2, delimiter=',')
 			file2=list(file2)
 		
-			with open('file1.csv') as csvfile1:
+			with open('file1new.csv') as csvfile1:
 				file1 = csv.reader(csvfile1, delimiter=',') 
 
 				# vecs=[]
@@ -119,13 +227,14 @@ def main():
 					arguments_review=[ file2[index] for index in [i for i, e in enumerate(file2) if e[0] == line[0] and e[-1]=="Review"] ]
 					arguments_reply=[ file2[index] for index in [i for i, e in enumerate(file2) if e[0] == line[0] and e[-1]=="Reply"] ]
 					# print(line)
+					# print(arguments_review)
 					sep=line[1].find("——————【reply】——————")
 					review=line[1][22:sep-2]
 					reply=line[1][sep+21:-1]
 					review=sep_sent(review)
-					tagging_sequence(outputfile,review,arguments_review,'Review')
+					tagging_sequence(outputfile,review,arguments_review,'Review',line[-1])
 					reply=sep_sent(reply)
-					tagging_sequence(outputfile,reply,arguments_reply,'Reply')
+					tagging_sequence(outputfile,reply,arguments_reply,'Reply',line[-1])
 					outputfile.write('\n')
 
 					# sents=rvw_sents+rpl_sents
@@ -168,9 +277,34 @@ def calc_vec(sents):
 	return all_vecs
 
 def sep_data():
-	with open("ReviewRebuttalnew.txt", "r") as full_data:
+	with open("ReviewRebuttalnew2.txt", "r") as full_data:
 		full_data = full_data.read().split('\n\n')
 		print(len(full_data))
+		
+		# paperid2pair = defaultdict(list)
+		# for passage_pair in full_data:
+		# 	id = passage_pair.split('\t')[-1]
+		# 	paperid2pair[id].append(passage_pair)
+
+		# # paperid2pair = {passage_pair.split('\t')[-1]: passage_pair for passage_pair in full_data}
+		# paperids = list(paperid2pair.keys())
+
+		# random.seed(230)
+		# random.shuffle(paperids)
+
+		# split_1 = int(0.82*len(paperids))
+		# split_2 = int(0.9*len(paperids))
+		# train_ids = paperids[:split_1]
+		# dev_ids = paperids[split_1:split_2]
+		# test_ids = paperids[split_2:]
+		
+		# train_data = [passage for id in train_ids for passage in paperid2pair[id]]
+		# dev_data = [passage for id in dev_ids for passage in paperid2pair[id]]
+		# test_data = [passage for id in test_ids for passage in paperid2pair[id]]
+
+		# print('ratio for train dev test: {}: {}: {}'.format(len(train_data), len(dev_data), len(test_data)))
+		
+
 		full_data.sort()  # make sure that the filenames have a fixed order before shuffling
 		random.seed(230)
 		random.shuffle(full_data)  # shuffles the ordering of filenames (deterministic given the chosen seed)
@@ -271,7 +405,218 @@ def generate_testnew():
 	f.close()
 	fnew.close()
 
+def get_paper_id():
+	fnew=open('ReviewRebuttalnew2.txt','r').readlines()
+	fold=open('ReviewRebuttalnew.txt','r').readlines()
+	fout = open('RR.txt','w')
+	for i in range(len(fold)):
+		if fold[i]!='\n':
+			tmp=fold[i][:-1]+'\t'+fnew[i].split('\t')[-1]
+			fout.write(tmp)
+		else:
+			fout.write('\n')
+	fout.close()
 
-# main()
-sep_data()
+def combine():
+	fnew=open('train1.txt','r').readlines()
+	fold=open('train.txt','r').readlines()
+	fout = open('checkI2B.txt','w')
+	count=0
+	for i in range(len(fnew)-1):
+		if fnew[i]!='\n' and fnew[i+1]!='\n':
+			if fold[i].split('\t')[1][0]=='O' and fnew[i].split('\t')[1][0]=='B' \
+			and (fold[i+1].split('\t')[1][0]!='B' or fnew[i+1].split('\t')[1][0]!='I'):
+				count+=1
+				print(fnew[i])
+				tmp=fnew[i].split('\t')[0]+'\t'+fold[i].split('\t')[1] \
+				+ '\t' + fold[i].split('\t')[2] \
+				+ '\t' + fold[i].split('\t')[3][:-1] \
+				+ '\t' + fnew[i].split('\t')[-1]
+				fout.write(tmp)
+			else:
+				fout.write(fnew[i])
+		else:
+			fout.write(fnew[i])
+	print(count)
+	fout.close()
+
+def match_diff_level():
+    file_seg = open('ReviewRebuttalnew2.txt','r').read().split('\n\n')
+    output = open('ReviewRebuttalnew3.txt','w')
+
+    # file1 = csv.reader(open('file1.csv'), delimiter=',')
+        
+        # print(len(file_seg),len(file1))
+    for idx,i in enumerate(file_seg):
+        # print(idx)
+        # print(i.split('\n')[0].split('\t')[0])
+        # print(list(file1))
+        flag =0
+        with open('file1.csv') as csvfile1:
+            file1 = csv.reader(csvfile1, delimiter=',') 
+            for j in file1:
+                # print(j[1])
+                # print(j)
+                if len(i.split('\n')[0].split('\t')[0])>30 and j[1].find(i.split('\n')[0].split('\t')[0].replace('[line_break_token]','\n').split('et al')[0])!=-1:
+                    for line in i.split('\n'):
+                        # print(line)
+                        line+='\t'
+                        line+=j[0]
+                        line+='\n'
+                        output.write(line)
+                    output.write('\n')
+                    flag +=1 
+                    break
+                else:
+                    # print(i.split('\n')[0].split('\t')[0].replace('[line_break_token]',''))
+                    if j[1].find(i.split('\n')[1].split('\t')[0].replace('[line_break_token]','\n').split('et al')[0])!=-1:
+                        for line in i.split('\n'):
+                            # print(line)
+                            line+='\t'
+                            line+=j[0]
+                            line+='\n'
+                            output.write(line)
+                        output.write('\n')
+                        flag += 1
+                        break
+                    # else:
+                        # print(i.split('\n')[0].split('\t')[0].replace('[line_break_token]',''))
+        if flag !=1:
+            print(flag,idx)
+            print(i.split('\n')[0].split('\t')[0])
+
+    output.close()
+
+def add_id():
+	file1 = open('RR.txt','r').readlines()
+	csvfile2 = open('file2.csv')
+	file2 = csv.reader(csvfile2, delimiter=',')
+	file2 = list(file2)
+	output = open('ReviewRebuttalnew3.txt','w')
+	id = 0
+	for i in range(len(file1)):
+		if file1[i]!='\n':
+			output.write(file2[id][0] + '\t' + file1[i])
+		else:
+			id+=2
+			output.write(file1[i])
+
+
+
+
+def update_label():
+	with open('ReviewRebuttalnew2.txt', 'w') as outputfile:
+
+		csvfile2 = open('file2.csv')
+		file2 = csv.reader(csvfile2, delimiter=',')
+		file2=list(file2)
+
+		for i in range(len(file2)):
+			file2[i][1]=file2[i][1].replace('by Yu et al.', 'by Yu et al..').replace('by Wen et al.', 'by Wen et al..').replace('et. al.','et al').replace('et al.,','et al').replace('et al.','et al').replace('\n','[line_break_token]').replace('\t','[tab_token]').split('.')[0]
+	
+		original =open('ReviewRebuttalnew3.txt','r').readlines()
+		flag=0
+		next_flag=0
+	
+		for i in range(len(original)-1):
+			flag=0
+
+			if next_flag==1:
+				next_flag=0
+				continue
+
+			if original[i]=='\n':
+				outputfile.write(original[i])
+				continue
+
+			if original[i].split('\t')[2]=='O':
+				file2_filter = [ file2[index] for index in [i for i, e in enumerate(file2) if e[0] == original[i].split('\t')[0] ] ]
+				# print(type(file2_filter))
+				for j in range(len(file2_filter)):
+					# print(file2[j][0], original[i].split('\t')[0])
+					a = file2_filter[j][1].replace('[line_break_token]','').replace('[tab_token]','')
+					b = original[i].split('\t')[1].replace('[line_break_token]','').replace('[tab_token]','')
+					if a in b and len(a)>=10:
+						label='B-'+file2_filter[j][2]
+						outputfile.write(original[i].split('\t')[1]+'\t'\
+							+ label + '\t' \
+							+ 'B-' + original[i].split('\t')[4] +'\t' \
+							+ original[i].split('\t')[4] +'\t' \
+							+ original[i].split('\t')[5])
+						print("--------a--------:",a,"--------b--------:",b)
+						flag=1
+
+						if original[i].split('\t')[2]==label:
+							next_label='I-'+file2_filter[j][2]
+							outputfile.write(original[i+1].split('\t')[1]+'\t'\
+								+ nextlabel + '\t' \
+								+ 'I-' + original[i+1].split('\t')[4] +'\t' \
+								+ original[i+1].split('\t')[4] +'\t' \
+								+ original[i+1].split('\t')[5])
+							next_flag=1
+			if flag==0:
+				outputfile.write(original[i])
+
+
+def match_train():
+	file_all = open('ReviewRebuttalnew2.txt','r').readlines()
+	file_part = open('train.txt','r').readlines()
+	file_new = open('train1.txt','w')
+	for i in file_part:
+		if i!='\n':
+			for j in file_all:
+				if i.split('\t')[0] == j.split('\t')[0] \
+				and i.split('\t')[-1] == j.split('\t')[-1] \
+				and i.split('\t')[-2] == j.split('\t')[-2]:
+					file_new.write(j)
+					break
+		else:
+			file_new.write(i)
+
+from collections import defaultdict
+def match_train_fast():
+	file_all = open('ReviewRebuttalnew2.txt','r').readlines() # confirm correct
+	file_old = open('test.txt','r').readlines() # Old, might be wrong
+	file_new = open('test1.txt','w') # follow the same order, but the content is from file_all
+
+	dict_all = defaultdict(list)
+	for i in range(len(file_all)):
+		dict_all[tuple(file_all[i].split('\t')[-2:])].append(i)
+	for line in file_old:
+		flag = False
+		for ind in dict_all[tuple(line.split('\t')[-2:])]: # iterate over limited indices of correct file
+			if file_all[ind].split('\t')[0].strip() == line.split('\t')[0].strip():
+				file_new.write(file_all[ind])
+				flag = True
+				break
+		if not flag:
+			print('not found: ', line)
+
+def check_wrong():
+	csvfile2 = open('file2.csv')
+	file2 = csv.reader(csvfile2, delimiter=',')
+	file2 = list(file2)
+	wrong_arguments = [row[1] for row in file2 if re.findall(r'^\S+[.]\s+', row[1])]
+	print(wrong_arguments)
+	print('count: ', len(wrong_arguments))
+
+
+if __name__=='__main__':
+	# argument = '''. What is missing is better motivating/explaining why these results are interesting/relevant. Sure, the GDBM reproduces certain experimental findings. But have we learned something new about the brain? Is the GDBM particularly well suited as a general model of visual cortex? Does the model make predictions? What about alternative, perhaps simpler models that could have been used instead? Etc.Also, are there related models or theoretical approaches to spontaneous activity?'''
+	# argument = re.sub(r'^. ', '', argument)
+	# print(argument)
+	# print(sep_sent(''', sparse DBNs show worse match to the biological findings as reported in [1]. A quantitative comparison between centered GDBMs and sparse DBNs is still open for future studies.' Where was it shown to be a worse match then?'''))	
+	# main()
+	# sep_sent('Done')
+	# check_wrong()
+	sep_data()
+	# get_paper_id()
+	# print(sep_sent('''.	We have replaced ‚ÄúWord frequency‚Äù with ‚ÄúWord ranking by frequency‚Äù'''))
+	# print(sep_sent('''Details about the corresponding association and communication events in the two datasets are provided in Appendix E.1. We uploaded a revised version that contains your suggested changes.'''))
+	# combine()
+	# update_file1()
+	# match_diff_level()
+	# add_id()
+	# update_label()
+	# match_train_fast()
 
